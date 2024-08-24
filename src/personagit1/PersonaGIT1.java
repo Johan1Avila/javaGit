a12353f402bb9ebe94426abc00ea18fd816cb8ed
Johan1Avila <johanavilagit@gmail.com>
2024-08-23 21:56:21 -0500

    primer commit en clase persona

---
+++ src/personagit1/PersonaGIT1.java
@@ -0,0 +1,10 @@
+
+package personagit1;
+public class PersonaGIT1 {
+
+    public static void main(String[] args) {
+        Persona persona1= new Persona("Johan",22);
+        persona1.saludar();
+    }
+    
+}
